413ebd5b00098dff2a8cecbbdd865ca4c1b3e8fc
Jens Edhammer <tobyndax@gmail.com>
2021-11-07 13:37:56 +0100

    adding test project

---
+++ tests/cpplib_test.cpp
@@ -0,0 +1,14 @@
+#include "src/lib/cpplib.h"
+
+#include "gtest/gtest.h"
+
+TEST(CPPLibTest, ReturnHelloWorld){
+  MyHelloClass testInstance;
+
+  testing::internal::CaptureStdout();
+  
+  testInstance.sayHello();
+
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_EQ("Hello, world!\n", output);    
+}
